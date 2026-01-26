d4cdc67b56b6e539dc4d3693fcc67b7b222b27e8
keresohirdetesek-afk <keresohirdetesek@gmail.com>
2026-01-26 22:05:45 +0100

    Update cb-radio-server.js

--- cb-radio-server.js
+++ cb-radio-server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('.'));
+
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
